b922988aaaf1831336c570e8eb23d1a05c1956cb
KAg5 <deepaknaidu1305@gmail.com>
2026-04-18 11:59:11 +0530

    Update port-scanner.py

--- port-scanner.py
+++ port-scanner.py
@@ -14,17 +14,17 @@ def scan_port(ip, port):
 def run_scanner(ip):
     open_ports = []
     print(f"Scanning target: {ip} (Ports 1-1000)...\n")
-    with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
-       
-        futures = {executor.submit(scan_port, ip, port): port for port in range(1, 1001)}
+    executor = concurrent.futures.ThreadPoolExecutor(max_workers=100)
+        # Submit tasks to the thread pool
+    futures = {executor.submit(scan_port, ip, port): port for port in range(1, 1001)}
         
-      
-        for future in concurrent.futures.as_completed(futures):
-            result = future.result()
-            if result is not None:
-                print(f"[+] Port {result} is OPEN")
-                open_ports.append(result)
-                
+        # as_completed yields futures as soon as they finish, rather than waiting for all
+    for future in concurrent.futures.as_completed(futures):
+        result = future.result()
+        if result is not None:
+            print(f"[+] Port {result} is OPEN")
+            open_ports.append(result)
+    executor.shutdown(wait=True)            
     print("\nScan complete.")
 
 if __name__ == "__main__":
